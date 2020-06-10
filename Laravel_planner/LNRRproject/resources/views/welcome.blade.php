ce03cba3b4ff712127e35db20bcca45957cc6cc0
Ricardo <81460@rocteraa-student.nl>
2020-06-10 11:28:05 +0200

    10 juni 2020
    welcome.blade ricardo ter linde

--- Laravel_planner/LNRRproject/resources/views/welcome.blade.php
+++ Laravel_planner/LNRRproject/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LNNR
                 </div>
 
                 <div class="links">
