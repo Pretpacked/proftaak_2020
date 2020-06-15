eb5dba68f12761d260c90ee6095bfb7e0f3f8822
Ricardo <81460@rocteraa-student.nl>
2020-06-15 09:46:08 +0200

    15 juni 2020
    Ricardo Linde home screen

--- Laravel_planner/LNRRproject/resources/views/welcome.blade.php
+++ Laravel_planner/LNRRproject/resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Planner</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
@@ -79,21 +79,14 @@
                 </div>
             @endif
 
-            <a href="covid">
-            <div class="content">
-                <div class="title m-b-md">
-                    LNNR
-                </div></a>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="content">
+                <div class=" m-b-md">
+                    <h1>Welkom op onze examen planner</h1>
+                    <h2>Login of registreer u om verder te gaan!</h2>
                 </div>
+
+
             </div>
         </div>
     </body>
