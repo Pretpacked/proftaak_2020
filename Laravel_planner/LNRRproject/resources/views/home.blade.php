2b1c06358175aedb5b5d14b3760d66f779d48806
Ricardo <81460@rocteraa-student.nl>
2020-06-15 13:23:25 +0200

    sidebar ricardo linde
    15 juni 2020

--- Laravel_planner/LNRRproject/resources/views/home.blade.php
+++ Laravel_planner/LNRRproject/resources/views/home.blade.php
@@ -10,14 +10,14 @@
                         </div>
                     @endif
 
-                    <div class="menu_field">
-                        <ul>
-                            <li>Plannen</li>
-                            <li class="list">Rooster</li>
-                            <li class="list">Studie</li>
-                            <li class="list">Covid-19</li>
-                        </ul>
+                <!-- Sidebar -->
+                    <div class="w3-sidebar w3-light-grey w3-bar-block sidebar" style="width:25%">
+                        <h3 class="w3-bar-item">Menu</h3>
+                        <a href="#" class="w3-bar-item w3-button">Link 1</a>
+                        <a href="#" class="w3-bar-item w3-button">Link 2</a>
+                        <a href="#" class="w3-bar-item w3-button">Link 3</a>
                     </div>
+
 
                 </div>
             </div>
--- Laravel_planner/LNRRproject/resources/views/layouts/app.blade.php
+++ Laravel_planner/LNRRproject/resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
