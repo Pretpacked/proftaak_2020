d8dac989102c9d59653773bd277911e53de0433b
Ricardo <81460@rocteraa-student.nl>
2020-06-17 12:08:52 +0200

    covid rules
    Ricardo linde

--- Laravel_planner/LNRRproject/resources/views/pages/covid.blade.php
+++ Laravel_planner/LNRRproject/resources/views/pages/covid.blade.php
@@ -19,6 +19,15 @@
                         <a href="covid" class="w3-bar-item w3-button">Covid 19</a>
                     </div>
 
+                    <div class="card covidrules" style="width: 30rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">Covid-19</h5>
+                          <h6 class="card-subtitle mb-2 text-muted">Rules</h6>
+                          <p class="card-text">De examens gaan dit jaar wat anders dan verwacht, zo moet je verplicht de 1,5 meter handteren bij ons op school en moet je bij binnekomst je handen desinfecteren.
+                              Voor de examens die gemaakt moeten worden zijn speciale lokalen vrij gehouden, bij deze examens mogen maar maximaal 20 leerling deelnemen.
+                          </p>
+                        </div>
+                      </div>
 
                 </div>
             </div>
